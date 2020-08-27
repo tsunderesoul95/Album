6bb1ef1931577181186eaa4ead55709698529500
tsunderesoul <tsunderesoul952gmail.com>
2020-08-27 13:14:07 +0530

    latest code

---
+++ app/src/main/java/com/example/retrofitdemo/RetrofitInstance.kt
@@ -0,0 +1,20 @@
+package com.example.retrofitdemo
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+         val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+        fun getRetrofitInstance() : Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+        }
+    }
+}
